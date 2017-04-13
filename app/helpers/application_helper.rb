8431e0f273c0fcf27889d4d68529e6242fcbd6cf
TuanLe263 <tuanlm263ptit@gmail.com>
2017-04-13 23:31:46 +0700

    Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title = ""
-    base_title = I18n.t("ror")
+    base_title = t "ror_app"
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
 end
