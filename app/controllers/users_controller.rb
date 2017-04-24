bb6a17484a8cc1ce2393a763e4ea088d9bff7916
TuanLe263 <tuanlm263ptit@gmail.com>
2017-04-24 09:15:57 +0700

    Chapter 10 implement updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :new, :create]
+  before_action :admin_user, only: :destroy
+  before_action :find_user, except: [:new, :create, :index]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
 
   def show
-    @user = User.find_by_id params[:id]
-    unless @user
-      flash[:success] = t "data_not_exist"
-      redirect_to root_path
-    end
   end
 
   def new
@@ -23,9 +26,51 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "profile"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] =  t "user_del"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "mess_login"
+      redirect_to login_path
+    end
+  end
+
+  def correct_user
+    redirect_to root_path unless @user.current_user? current_user
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:success] = t "data_not_exist"
+      redirect_to root_path
+    end
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
 
-class UserLoginTest < ActionDispatch::IntegrationTest
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
   test "login with invalid information" do
     get login_path
     assert_template "sessions/new"
-    post login_path, params: {session: {email: "", password: ""}}
+    # post login_path, params: {sessions: {email: "", password: ""}}
     assert_template "sessions/new"
     assert_not flash.empty?
     get root_path
