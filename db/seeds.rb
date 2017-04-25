ad0511e6e6670cd66aae7a67b06f6bef4f9e3976
TuanLe263 <tuanlm263ptit@gmail.com>
2017-04-25 23:15:57 +0700

    Chapter 13 User-Microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create! name:  "Example User", email: "example@railstutorial.org",
-  password: "foobar", password_confirmation: "foobar", admin: true,
+  password: "123456", password_confirmation: "123456", admin: true,
   activated: true, activated_at: Time.zone.now
 
 49.times do |n|
@@ -16,4 +16,9 @@ User.create! name:  "Example User", email: "example@railstutorial.org",
   User.create! name: name, email: email, password: password,
     password_confirmation: password, activated: true,
     activated_at: Time.zone.now
+end
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
